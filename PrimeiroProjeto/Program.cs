6c2f80f60febfc04e37418e20c24ff15056ae6c9
francovinicius <duartevini.dev@gmail.com>
2023-08-02 15:48:28 -0300

    alt string para int

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -24,6 +24,15 @@ void ExibirOpcoesDoMenu ()
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média de uma banda");
     Console.WriteLine("Digite 0 para sair");
+
+    Console.Write("\nDigite a opção desejada:");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    if(opcaoEscolhidaNumerica == 1)
+    {
+        Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+    }
 }
 
 ExibirMensagemBoasVindas();
