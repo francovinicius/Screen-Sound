772f384b3cd7a4ae18e8ca8d70bf00f7aa5184ed
francovinicius <duartevini.dev@gmail.com>
2023-08-02 15:16:33 -0300

    first commit

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,11 @@
+﻿//Screen Sound
+string mensagemBoasVindas = "Boas Vindas ao app";
+
+void ExibirMensagemBoasVindas()
+{
+    Console.WriteLine("***************************");
+    Console.WriteLine(mensagemBoasVindas);
+    Console.WriteLine("***************************");
+}
+
+ExibirMensagemBoasVindas();
