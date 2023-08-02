c1e946260619bdf04a53fc3f2cd873bd99dedf24
francovinicius <duartevini.dev@gmail.com>
2023-08-02 15:41:25 -0300

    add opções

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -3,9 +3,28 @@ string mensagemBoasVindas = "Boas Vindas ao app";
 
 void ExibirMensagemBoasVindas()
 {
-    Console.WriteLine("***************************");
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+
     Console.WriteLine(mensagemBoasVindas);
-    Console.WriteLine("***************************");
+}
+
+void ExibirOpcoesDoMenu ()
+{
+    //\n pra dar espaço
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite 0 para sair");
 }
 
 ExibirMensagemBoasVindas();
+ExibirOpcoesDoMenu();
