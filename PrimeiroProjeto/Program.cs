b79e85f6c18a507c69f01dee34667610d3e36222
francovinicius <duartevini.dev@gmail.com>
2023-08-02 15:54:54 -0300

    add lista

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -23,15 +23,38 @@ void ExibirOpcoesDoMenu ()
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média de uma banda");
-    Console.WriteLine("Digite 0 para sair");
+    Console.WriteLine("Digite -1 para sair");
 
     Console.Write("\nDigite a opção desejada:");
     string opcaoEscolhida = Console.ReadLine()!;
     int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
 
+    /*
+     * 
     if(opcaoEscolhidaNumerica == 1)
     {
         Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+    } else if (opcaoEscolhidaNumerica == 2)
+    {
+        Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+    }
+
+    */
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+            break;
+        case 2: Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+            break;
+        case 3: Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+            break;
+        case 4: Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+            break;
+        case -1: Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+            break;
+        default: Console.WriteLine("Opção invalida");
+            break;
     }
 }
 
